d53945eb73188f86a85f8f4062fc36af7de20cb8
webquest-nz <glenn@webquest.co.nz>
2026-03-04 14:49:05 +1300

    Update manifest file

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 from codecs import open
 from os import path
@@ -8,7 +8,7 @@ from wagtailcolourpicker import __version__
 
 
 install_requires = [
-    'wagtail>=2'
+    'wagtail>=4,<6'
 ]
 
 documentation_extras = [
@@ -28,19 +28,19 @@ setup(
     version=__version__,
     description='Wagtail text colour picker for DraftJS',
     long_description=long_description,
+    long_description_content_type='text/x-rst',
     author='Stuart George',
     author_email='stuart@accentdesign.co.uk',
     url='https://github.com/AccentDesign/wagtailcolourpicker/',
-    download_url='https://pypi.python.org/pypi/wagtailcolourpicker',
+    download_url='https://pypi.org/project/wagtailcolourpicker/',
     license='MIT',
-    packages=[
-        'wagtailcolourpicker'
-    ],
+    packages=find_packages(include=['wagtailcolourpicker', 'wagtailcolourpicker.*']),
     install_requires=install_requires,
     extras_require={
         'docs': documentation_extras
     },
     include_package_data=True,
+    python_requires='>=3.8',
     keywords=['wagtail', 'draftjs', 'colour', 'picker', 'accent', 'design'],
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -48,13 +48,16 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Framework :: Django',
-        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 4.2',
         'Framework :: Wagtail',
-        'Framework :: Wagtail :: 2',
+        'Framework :: Wagtail :: 4',
+        'Framework :: Wagtail :: 5',
         'Topic :: Internet :: WWW/HTTP :: Site Management',
     ],
 )
